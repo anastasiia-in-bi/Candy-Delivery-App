c8d51b3848ba0099bd4c351bcdbf0e142c099013
Anastasiia Bordunova <Anastasiia_Batalova@epam.com>
2022-08-18 22:30:59 +0300

    fix linter

--- src/models/courier.py
+++ src/models/courier.py
@@ -6,7 +6,8 @@ class CourierManager(Manager):
         super(CourierManager, self).__init__(db_path)
 
     def create(self, name: str, age: int, courier_type: str) -> list:
-        add_sql = f"INSERT INTO courier(name, age, courier_type) VALUES('{name}', {age}, '{courier_type}') RETURNING *"
+        add_sql = f"INSERT INTO courier(name, age, courier_type) " \
+                  f"VALUES('{name}', {age}, '{courier_type}') RETURNING *"
 
         return self.execute_and_return(add_sql)
 
@@ -15,13 +16,17 @@ class CourierManager(Manager):
 
         return self.execute_and_return(select_couriers)
 
-    def update(self, courier_id: int, name: str, age: int, courier_type: str) -> list:
+    def update(
+            self, courier_id: int, name: str, age: int, courier_type: str
+    ) -> list:
         alter_courier = f"UPDATE courier SET name='{name}', age={age}, " \
-                        f"courier_type='{courier_type}' WHERE {courier_id}=courier_id RETURNING *;"
+                        f"courier_type='{courier_type}' " \
+                        f"WHERE {courier_id}=courier_id RETURNING *;"
 
         return self.execute_and_return(alter_courier)
 
     def delete(self, courier_id: int) -> list:
-        del_courier = f"DELETE FROM courier WHERE courier_id={courier_id} RETURNING *;"
+        del_courier = f"DELETE FROM courier " \
+                      f"WHERE courier_id={courier_id} RETURNING *;"
 
         return self.execute_and_return(del_courier)
